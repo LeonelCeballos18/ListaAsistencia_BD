7bfd4c2b834b50e04c5101c6c189f6ace3a0e3a9
LeonelCeballos18 <lceballos5@ucol.mx>
2024-10-21 23:16:45 -0700

    change auth.routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { signup, login, logout, checkAuth } from "../controllers/auth.controller";
 import { verifyToken } from "../middleware/verifyToken.js";
-import { log } from "console";
 
 const router = express.Router();
 
 router.get("/check-auth", verifyToken, checkAuth);
 
-router.post("/signup", signup);
+//router.post("/signup", signup);
 router.post("/login", login);
 router.post("logout", logout)
 
